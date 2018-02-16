e037926d6fddd0c9128ce2f29f38f2b7ab9e0107
Paul Steven <paul@mediakitchen.co.uk>
2018-02-16 16:12:02 +0000

    Update adapt-elfh-home-redirect.js
    Add code for toggling logo redirection on/off

--- js/adapt-elfh-home-redirect.js
+++ js/adapt-elfh-home-redirect.js
@@ -6,6 +6,7 @@ define(['core/js/adapt'], function(Adapt) {
       return;
 	
     var destURL = Adapt.config.get('_elfh_homeRedirect')._redirectURL;	
+    var boolRedirectLogo = Adapt.config.get('_elfh_homeRedirect')._redirectLogo;	
     var ScriptTag = document.createElement("script");	
 	
 	var RedirectScript = 'var applyHomeButton = function() {if (document.getElementsByClassName("navigation-home-button").length !== 0) { clearInterval(intervalId);   var button = window.$(".navigation-home-button")[0];  delete button.dataset.event; button.onclick = function(event) { event && event.preventDefault();  window.location = "' + destURL + '";  }; };  if (document.getElementsByClassName("navigation-logo-home-button").length !== 0) { clearInterval(intervalId); var button2 = window.$(".navigation-logo-home-button")[0]; delete button2.dataset.event; button2.onclick = function(event) { event && event.preventDefault(); window.location = "' + destURL + '"; };  }; };var intervalId = setInterval(applyHomeButton, 500);';
